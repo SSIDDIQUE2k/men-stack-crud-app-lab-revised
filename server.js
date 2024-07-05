a6590ea2e7cc13043c26b5eabe172f73a23e6b88
Shazib Siddique <siddiqueshazib4@gmail.com>
2024-07-05 15:29:54 -0400

    created a tax return page

--- server.js
+++ server.js
@@ -18,15 +18,17 @@ mongoose.connection.on("connected", () => {
     console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
   });
 
-// Import the Fruit model
-const Fruit = require("./models/fruit.js");
+// Import the ASI model
+const _ASI_= require("./models/a-s-i.js");
  
 // GET /
 app.get("/", async (req, res) => {
     res.render("index.ejs");
   });
-  
-
+//  Get /tax/new
+app.get("/taxes/new", async (req, res) => {
+    res.render("taxes/new.ejs");
+  });
 
 app.listen(3000, () => {
   console.log("Listening on port 3000");
