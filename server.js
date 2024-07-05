81fcf9ec8d5f8837cb4e9d2e9c4fc1cbc8d4cbe8
Shazib Siddique <siddiqueshazib4@gmail.com>
2024-07-05 13:33:34 -0400

    created a landing page

--- server.js
+++ server.js
@@ -4,6 +4,14 @@ const express = require("express");
 
 const app = express();
 
+// server.js
+
+// GET /
+app.get("/", async (req, res) => {
+    res.render("index.ejs");
+  });
+  
+
 app.listen(3000, () => {
   console.log("Listening on port 3000");
 });
